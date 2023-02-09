10b7f6bab2195e4a9580ee19036be8a990c73df2
Tai Zeng <zengtai@gmail.com>
2023-02-09 16:53:08 +0800

    加 build id、删ads.txt sitemap

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const nextConfig = {
   },
   trailingSlash: true,
   basePath: "/webs/gpbox/v5",
+
+  generateBuildId: async () => {
+    // You can, for example, get the latest git commit hash here
+    return "20230209";
+  },
 };
 
 module.exports = nextConfig;
